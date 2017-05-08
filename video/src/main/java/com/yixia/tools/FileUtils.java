4e08638471a565acaff4bf406073ba578ec0c3b9
unknown <>
2017-05-08 15:30:09 +0800

    aa

--- video/src/main/java/com/yixia/tools/FileUtils.java
+++ video/src/main/java/com/yixia/tools/FileUtils.java
@@ -1,6 +1,8 @@
 package com.yixia.tools;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
 import android.os.Environment;
 
 import java.io.File;
@@ -30,5 +32,33 @@ public class FileUtils {
             cachePath = mContext.getCacheDir().getPath();
         }
         return  cachePath + File.separator + dirName;
+    }
+
+    /**
+     * 其中函数getFrameAtTime()有其他重载函数，
+     * 该函数会随机选择一帧抓取，如果想要指定具体时
+     * 间的缩略图，可以用函数getFrameAtTime(long timeUs),
+     *  getFrameAtTime(long timeUs, int option)
+     * @param filePath
+     * @return
+     */
+    public static Bitmap getVideoThumbnail(String filePath) {
+        Bitmap bitmap = null;
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        try {
+            retriever.setDataSource(filePath);
+            bitmap = retriever.getFrameAtTime();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                retriever.release();
+            } catch (RuntimeException e) {
+                e.printStackTrace();
+            }
+        }
+        return bitmap;
     }
 }
